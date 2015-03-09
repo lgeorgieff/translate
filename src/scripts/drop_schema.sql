f798281a650fb745adba7b0da6fecd8405b5ce41
Lukas Georgieff <lukas.georgieff@hotmail.com>
2015-03-09 21:59:43 +0100

    Add the possibility to represent multiple word classes for one phrase in the DB

--- src/scripts/drop_schema.sql
+++ src/scripts/drop_schema.sql
@@ -1,7 +1,7 @@
 /*
 #######################################################################################################################
 # Copyright (C) 2015  Lukas Georgieff
-# Last modified: 06/03/2015
+# Last modified: 09/03/2015
 # Description: Drops the (postgresql) SQL data base schema for the server side of translate.
 #              Usage: psql --username translate --dbname translate --file drop_schema.sql
 #######################################################################################################################
@@ -23,6 +23,7 @@
 
 DROP TABLE phrase_translation CASCADE;
 DROP TABLE phrase_comment CASCADE;
+DROP TABLE phrase_word_class CASCADE;
 DROP TABLE phrase CASCADE;
 DROP TYPE numerus CASCADE;
 DROP TABLE language CASCADE;
