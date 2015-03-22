412253d769881fd68c3c173525013c3829d6063e
Lukas Georgieff <lukas.georgieff@hotmail.com>
2015-03-22 14:06:36 +0100

    Add unit test for src/utils/gender.hpp
    Add unit tests for the Gender enum class.
    Refactor the methods to_db_string and from_db_string, so that adding
    new values to Gender is made easier.

---
+++ src/test/utils/test_main.cpp
@@ -0,0 +1,24 @@
+// ====================================================================================================================
+// Copyright (C) 2015  Lukas Georgieff
+// Last modified: 02/22/2015
+// Description: Entry point for the utils unit tests.
+// ====================================================================================================================
+
+// ====================================================================================================================
+// This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public
+// License as published by the Free Software Foundation in version 2.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+// warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+// details.
+//
+// You should have received a copy of the GNU General Public License along with this program; if not, write to the
+// Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+// ====================================================================================================================
+
+#include "gtest/gtest.h"
+
+int main(const int argc, const char **argv) {
+  testing::InitGoogleTest(const_cast<int *>(&argc), const_cast<char **>(argv));
+  return RUN_ALL_TESTS();
+}
