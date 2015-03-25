8c260b25f1dda479c43575cc550df36eae68961c
Lukas Georgieff <lukas.georgieff@hotmail.com>
2015-03-25 22:27:10 +0100

    Fix a bug in the SQL queries for translations

--- src/server/db_query.cpp
+++ src/server/db_query.cpp
@@ -1,6 +1,6 @@
 // ====================================================================================================================
 // Copyright (C) 2015  Lukas Georgieff
-// Last modified: 03/24/2015
+// Last modified: 03/25/2015
 // Description: Implements the DB_Query class that allows to query the data base for language information.
 // ====================================================================================================================
 
@@ -32,8 +32,9 @@ DB_Query::DB_Query(const Connection_String& connection_string) : db_connection_{
   this->db_connection_ = new pqxx::connection(connection_string.to_string());
   this->connection_self_created_ = true;
 }
-  
-  DB_Query::DB_Query(pqxx::connection* db_connection) : db_connection_{db_connection}, query_result_{}, connection_self_created_{false} {
+
+DB_Query::DB_Query(pqxx::connection* db_connection)
+    : db_connection_{db_connection}, query_result_{}, connection_self_created_{false} {
   if (!this->db_connection_) throw DB_Exception("db_connection must not be a nullptr!");
 }
 
@@ -67,18 +68,18 @@ std::string DB_Query::generate_exact_match_query(const std::string& phrase_in, c
   ss << "select"
         " ph_in.language as language_in,"
         " ph_in.phrase as phrase_in,"
-        " wc_in.name as word_class_in,"
+        " wc_in.id as word_class_in,"
         " ph_in.gender as gender_in,"
         " ph_in.numerus as numerus_in,"
         " ab_in.abbreviation as abbreviation_in,"
         " co_in.comment as comment_in,"
         " ph_out.language as language_out,"
         " ph_out.phrase as phrase_out,"
-        " wc_out.name as word_class_out,"
+        " wc_out.id as word_class_out,"
         " ph_out.gender as gender_out,"
         " ph_out.numerus as numerus_out,"
         " ab_out.abbreviation as abbriviation_out,"
-        " co_out.comment as comment_out"
+        " co_out.comment as comment_out "
         "from phrase ph_in"
         " LEFT OUTER JOIN phrase_abbreviation pa_in ON ph_in.id = pa_in.phrase_id"
         " LEFT OUTER JOIN abbreviation ab_in ON ab_in.id = pa_in.abbreviation_id"
@@ -93,10 +94,10 @@ std::string DB_Query::generate_exact_match_query(const std::string& phrase_in, c
         " LEFT OUTER JOIN phrase_word_class pw_out ON ph_out.id = pw_out.phrase_id"
         " LEFT OUTER JOIN word_class wc_out ON wc_out.id = pw_out.word_class_id"
         " LEFT OUTER JOIN phrase_comment pc_out ON pc_out.phrase_id = ph_out.id"
-        " LEFT OUTER JOIN comment co_out ON co_out.id = pc_out.comment_id"
-        " where ph_in.phrase = '" << this->db_connection_->esc(phrase_in) << "' and ph_in.language = '"
-     << this->db_connection_->esc(language_in) << "' and ph_out.language = '" << this->db_connection_->esc(language_out)
-     << "';";
+        " LEFT OUTER JOIN comment co_out ON co_out.id = pc_out.comment_id "
+        "where ph_in.phrase = '" << this->db_connection_->esc(phrase_in) << "' and ph_in.language = '"
+     << this->db_connection_->esc(language_in) << "' and ph_out.language = '"
+     << this->db_connection_->esc(language_out) << "';";
   return ss.str();
 }
 
@@ -108,18 +109,18 @@ std::string DB_Query::generate_exact_match_word_class_query(const std::string& p
   ss << "select"
         " ph_in.language as language_in,"
         " ph_in.phrase as phrase_in,"
-        " wc_in.name as word_class_in,"
+        " wc_in.id as word_class_in,"
         " ph_in.gender as gender_in,"
         " ph_in.numerus as numerus_in,"
         " ab_in.abbreviation as abbreviation_in,"
         " co_in.comment as comment_in,"
         " ph_out.language as language_out,"
         " ph_out.phrase as phrase_out,"
-        " wc_out.name as word_class_out,"
+        " wc_out.id as word_class_out,"
         " ph_out.gender as gender_out,"
         " ph_out.numerus as numerus_out,"
         " ab_out.abbreviation as abbriviation_out,"
-        " co_out.comment as comment_out"
+        " co_out.comment as comment_out "
         "from phrase ph_in"
         " LEFT OUTER JOIN phrase_abbreviation pa_in ON ph_in.id = pa_in.phrase_id"
         " LEFT OUTER JOIN abbreviation ab_in ON ab_in.id = pa_in.abbreviation_id"
@@ -134,11 +135,11 @@ std::string DB_Query::generate_exact_match_word_class_query(const std::string& p
         " LEFT OUTER JOIN phrase_word_class pw_out ON ph_out.id = pw_out.phrase_id"
         " LEFT OUTER JOIN word_class wc_out ON wc_out.id = pw_out.word_class_id"
         " LEFT OUTER JOIN phrase_comment pc_out ON pc_out.phrase_id = ph_out.id"
-        " LEFT OUTER JOIN comment co_out ON co_out.id = pc_out.comment_id"
-        " where ph_in.phrase = '" << this->db_connection_->esc(phrase_in) << "' and ph_in.language = '"
-     << this->db_connection_->esc(language_in) << "' and ph_out.language = '" << this->db_connection_->esc(language_out)
-     << "' and wc_in.name = '" << this->db_connection_->esc(word_class) << "'and wc_out.name = '"
-     << this->db_connection_->esc(word_class) << "';";
+        " LEFT OUTER JOIN comment co_out ON co_out.id = pc_out.comment_id "
+        "where ph_in.phrase = '" << this->db_connection_->esc(phrase_in) << "' and ph_in.language = '"
+     << this->db_connection_->esc(language_in) << "' and ph_out.language = '"
+     << this->db_connection_->esc(language_out) << "' and wc_in.id = '" << this->db_connection_->esc(word_class)
+     << "'and wc_out.id = '" << this->db_connection_->esc(word_class) << "';";
   return ss.str();
 }
 
