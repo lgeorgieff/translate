b5f97941b946a66dda57ba69af250f582993cb0a
Lukas Georgieff <lukas.georgieff@hotmail.com>
2015-05-26 22:04:17 +0200

    Add the class HttPostRequest for performing HTTP POST requests

---
+++ src/client/http_post_request.hpp
@@ -0,0 +1,46 @@
+// ====================================================================================================================
+// Copyright (C) 2015  Lukas Georgieff
+// Last modified: 05/26/2015
+// Description: Declares a class for an HTTP POST request to the translation service.
+// ====================================================================================================================
+
+// ====================================================================================================================
+// This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public
+// License as published by the Free Software Foundation in version 2.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+// warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+// details.
+//
+// You should have received a copy of the GNU General Public License along with this program; if/ not, write to the
+// Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+// ====================================================================================================================
+
+#ifndef HTTP_POST_REQUEST_HPP_
+#define HTTP_POST_REQUEST_HPP_
+
+#include "http_request.hpp"
+
+#include <string>
+
+namespace lgeorgieff {
+namespace translate {
+namespace client {
+class HttpPostRequest : public HttpRequest {
+ public:
+  // === Constructors and destructor ==================================================================================
+  // ctor takes the HTTP request URL and the POST data
+  HttpPostRequest(const std::string &, const std::string&);
+  virtual ~HttpPostRequest();
+
+ protected:
+  // The actual method that handles the curl HTTP request and must be implemented in each derived class from
+  // HttpRequest
+  void do_request();
+  std::string post_data_;
+};  // HttpPostRequest
+}  // client
+}  // translate
+}  // lgeorgieff
+
+#endif  // HTTP_POST_REQUEST_HPP_
