f707dd4e3aa036a1f63db407b9aa24549f46f3be
Lukas Georgieff <lukas.georgieff@hotmail.com>
2015-05-26 21:31:01 +0200

    Add the class HttGetRequest for performing HTTP GET requests

---
+++ src/client/http_get_request.hpp
@@ -0,0 +1,44 @@
+// ====================================================================================================================
+// Copyright (C) 2015  Lukas Georgieff
+// Last modified: 05/26/2015
+// Description: Declares a class for an HTTP GET request to the translation service.
+// ====================================================================================================================
+
+// ====================================================================================================================
+// This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public
+// License as published by the Free Software Foundation in version 2.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+// warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+// details.
+//
+// You should have received a copy of the GNU General Public License along with this program; if/ not, write to the
+// Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+// ====================================================================================================================
+
+#ifndef HTTP_GET_REQUEST_HPP_
+#define HTTP_GET_REQUEST_HPP_
+
+#include "http_request.hpp"
+
+#include <string>
+
+namespace lgeorgieff {
+namespace translate {
+namespace client {
+class HttpGetRequest : public HttpRequest {
+ public:
+  // === Constructors and destructor ==================================================================================
+  HttpGetRequest(const std::string &);
+  virtual ~HttpGetRequest();
+
+ protected:
+  // The actual method that handles the curl HTTP request and must be implemented in each derived class from
+  // HttpRequest
+  void do_request();
+};  // HttpGetRequest
+}  // client
+}  // translate
+}  // lgeorgieff
+
+#endif  // HTTP_GET_REQUEST_HPP_
