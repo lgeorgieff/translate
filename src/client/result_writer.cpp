beb8349c1ab1310f5fb76bdaec279d794ab83cb8
Lukas Georgieff <lukas.georgieff@hotmail.com>
2015-06-03 21:45:44 +0200

    Add error handling to result writer

--- src/client/result_writer.cpp
+++ src/client/result_writer.cpp
@@ -1,6 +1,6 @@
 // ====================================================================================================================
 // Copyright (C) 2015  Lukas Georgieff
-// Last modified: 06/02/2015
+// Last modified: 06/03/2015
 // Description: Implements the class ResultWriter which provides functionality that writes the translation results
 //              (json strings) into a stream in a pretty format.
 // ====================================================================================================================
@@ -19,6 +19,7 @@
 
 #include "result_writer.hpp"
 #include "utils/helper.hpp"
+#include "utils/json_exception.hpp"
 
 #include "json/json.h"
 
@@ -27,19 +28,19 @@
 #include <set>
 
 namespace {
+// Returns a json value the is the pared result of the give json string.
 Json::Value from_json_string(const std::string json_string) {
-  try {
-    Json::Reader reader;
-    Json::Value root;
-    if (!reader.parse(json_string, root)) throw 1;  // TODO: implement error handling
-    return root;
-  } catch (...) {  // TODO: check exception type
-    throw 1;       // TODO: implement error handling
-  }
+  Json::Reader reader;
+  Json::Value root;
+  if (!reader.parse(json_string, root))
+    throw lgeorgieff::translate::utils::JsonException{"The json data " + json_string + " cannot be parsed!"};
+  return root;
 }
 
-void write_item_to(const std::string& data, size_t max_length, size_t tab_space, std::ostream* destination,
-                   bool is_last_item) {
+// A helper function that writes the passed item into the ostream target.
+// The written string is checked for length and adapted to the defined max_length.
+void write_item_to_stream(const std::string& data, size_t max_length, size_t tab_space, std::ostream* destination,
+                          bool is_last_item) {
   size_t local_tab_space;
   size_t local_max_length;
   if (is_last_item) {
@@ -47,7 +48,6 @@ void write_item_to(const std::string& data, size_t max_length, size_t tab_space,
   } else {
     local_tab_space = tab_space;
     local_max_length = max_length;
-    ;
   }
   ssize_t padding;
   if (local_max_length + local_tab_space < data.size())
@@ -61,6 +61,8 @@ void write_item_to(const std::string& data, size_t max_length, size_t tab_space,
 namespace lgeorgieff {
 namespace translate {
 namespace client {
+
+using lgeorgieff::translate::utils::JsonException;
 
 const size_t ResultWriter::TAB_SPACE{4};
 
@@ -90,14 +92,15 @@ void ResultWriter::write_json_array_string(const std::string& json_string,
                                            const std::vector<std::string>& object_members, bool write_headers) {
   Json::Value data{from_json_string(json_string)};
   if (data.isNull()) return;
-  if (!data.isArray()) throw 1;  // TODO: implement error handling
+  if (!data.isArray()) throw JsonException{"Cannot process the json data " + json_string + ". Expected a json array!"};
   // We just go through the data and check for completeness, validity and chek the max length of each member type.
   // We don't write into the set ostream here until it is clear that all data is valid.
   std::map<std::string, size_t> max_lengths;
   for (const Json::Value& item : data) {
-    if (!item.isObject()) throw 1;  // TODO: implement error handling
+    if (!item.isObject()) throw JsonException{"Cannot process the json data " + item.toStyledString() + ". Expected a json object!"};
     for (const std::string member_name : object_members) {
-      if (!item.isMember(member_name) || !item[member_name].isString()) throw 1;  // TODO: implement error handling
+      if (!item.isMember(member_name) || !item[member_name].isString())
+        throw JsonException{"Cannot process the json data " + item[member_name].toStyledString() + ". Expected a json string!"};
       if (max_lengths.end() == max_lengths.find(member_name) ||
           max_lengths[member_name] < item[member_name].asString().size()) {
         max_lengths[member_name] = item[member_name].asString().size();  // TODO: handle unicode string length
@@ -111,9 +114,9 @@ void ResultWriter::write_json_array_string(const std::string& json_string,
       if (max_lengths[member_name] < member_name.size()) max_lengths[member_name] = member_name.size();
 
     for (size_t member_pos{0}; member_pos != object_members.size(); ++member_pos) {
-      write_item_to(lgeorgieff::translate::utils::to_upper_case(object_members[member_pos]),
-                    max_lengths[object_members[member_pos]], TAB_SPACE, this->destination_,
-                    member_pos == object_members.size() - 1);
+      write_item_to_stream(lgeorgieff::translate::utils::to_upper_case(object_members[member_pos]),
+                           max_lengths[object_members[member_pos]], TAB_SPACE, this->destination_,
+                           member_pos == object_members.size() - 1);
     }
     *(this->destination_) << std::endl;
   }
@@ -121,9 +124,9 @@ void ResultWriter::write_json_array_string(const std::string& json_string,
   // Write the data to its destination.
   for (size_t array_pos{0}; array_pos != data.size(); ++array_pos) {
     for (size_t member_pos{0}; member_pos != object_members.size(); ++member_pos) {
-      write_item_to(data[static_cast<int>(array_pos)][object_members[member_pos]].asString(),
-                    max_lengths[object_members[member_pos]], TAB_SPACE, this->destination_,
-                    member_pos == object_members.size() - 1);
+      write_item_to_stream(data[static_cast<int>(array_pos)][object_members[member_pos]].asString(),
+                           max_lengths[object_members[member_pos]], TAB_SPACE, this->destination_,
+                           member_pos == object_members.size() - 1);
     }
     *(this->destination_) << std::endl;
   }
@@ -144,7 +147,7 @@ void ResultWriter::write_genders(const std::string& genders) {
 void ResultWriter::write_numeri(const std::string& numeri) {
   Json::Value data{from_json_string(numeri)};
   if (data.isNull()) return;
-  if (!data.isArray()) throw 1;  // TODO: implement error handling
+  if (!data.isArray()) throw JsonException{"Cannot process the json data " + numeri + ". Expected a json array!"};
 
   *(this->destination_) << "NUMERUS" << std::endl;
   for (const Json::Value numerus : data) *(this->destination_) << numerus.asString() << std::endl;
@@ -153,7 +156,7 @@ void ResultWriter::write_numeri(const std::string& numeri) {
 void ResultWriter::write_json_string_string(const std::string& json_string) {
   Json::Value data{from_json_string(json_string)};
   if (data.isNull()) return;
-  if (!data.isString()) throw 1;  // TODO: implement error handling
+  if (!data.isString()) throw JsonException{"Cannot process the json data " + json_string + ". Expected a json string!"};
   *(this->destination_) << data.asString() << std::endl;
 }
 
@@ -184,46 +187,53 @@ void ResultWriter::write_translation(const std::string& translation) {
   // [<COMMENT-N>]
   Json::Value data{from_json_string(translation)};
   if (data.isNull()) return;
-  if (!data.isArray()) throw 1;  // TODO: implement error handling
+  if (!data.isArray()) throw JsonException{"Cannot process the json data " + translation + ". Expected a json array!"};
 
   for (const Json::Value& item : data) {
     if (item.isNull()) continue;
-    if (!item.isObject()) throw 1;  // TODO: implement error handling
+    if (!item.isObject())
+      throw JsonException{"Cannot process the json data " + item.toStyledString() + ". Expected a json object!"};
     std::string phrase;
     if (item.isMember("phrase") && !item["phrase"].isNull()) {
-      if (!item["phrase"].isString()) throw 1;
+      if (!item["phrase"].isString()) {
+        throw JsonException{"Cannot process the json data " + item["phrase"].toStyledString() +
+                            ". Expected a json string!"};
+      }
       phrase = item["phrase"].asString();
     }
     std::string word_class;
     if (item.isMember("word_class") && !item["word_class"].isNull()) {
-      if (!item["word_class"].isString()) throw 1;  // TODO: implement error handling
+      if (!item["word_class"].isString()) throw JsonException{"Cannot process the json data " + item["word_class"].toStyledString() + ". Expected a json string!"};
       word_class = item["word_class"].asString();
     }
     std::string gender;
     if (item.isMember("gender") && !item["gender"].isNull()) {
-      if (!item["gender"].isString()) throw 1;  // TODO: implement error handling
+      if (!item["gender"].isString()) throw JsonException{"Cannot process the json data " + item["gender"].toStyledString() + ". Expected a json string!"};
       gender = item["gender"].asString();
     }
     std::string numerus;
     if (item.isMember("numerus") && !item["numerus"].isNull()) {
-      if (!item["numerus"].isString()) throw 1;  // TODO: implement error handling
+      if (!item["numerus"].isString()) throw JsonException{"Cannot process the json data " + item["numerus"].toStyledString() + ". Expected a json string!"};
       numerus = item["numerus"].asString();
     }
     std::set<std::string> abbreviations;
     if (item.isMember("abbreviations") && !item["abbreviations"].isNull()) {
-      if (!item["abbreviations"].isArray()) throw 1;  // TODO: implement error handling
+      if (!item["abbreviations"].isArray()) throw JsonException{"Cannot process the json data " + item["abbreviations"].toStyledString() + ". Expected a json array!"};
       for (const Json::Value& abbreviation : item["abbreviations"]) {
         if (abbreviation.isNull()) continue;
-        if (!abbreviation.isString()) throw 1;  // TODO: implement error handling
+        if (!abbreviation.isString()) throw JsonException{"Cannot process the json data " + abbreviation.toStyledString() + ". Expected a json string!"};
         abbreviations.insert(abbreviation.asString());
       }
     }
     std::set<std::string> comments;
     if (item.isMember("comments") && !item["comments"].isNull()) {
-      if (!item["comments"].isArray()) throw 1;  // TODO: implement error handling
+      if (!item["comments"].isArray()) {
+        throw JsonException{"Cannot process the json data " + item["comments"].toStyledString() +
+                            ". Expected a json array!"};
+      }
       for (const Json::Value& comment : item["comments"]) {
         if (comment.isNull()) continue;
-        if (!comment.isString()) throw 1;  // TODO: implement error handling
+        if (!comment.isString()) throw JsonException{"Cannot process the json data " + comment.toStyledString() + ". Expected a json string!"};
         comments.insert(comment.asString());
       }
     }
